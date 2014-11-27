e0e123c4ec9fcf62892f6945926c708c7f475d73
sushithbalu <sushith1132@gmail.com>
2014-11-27 21:57:24 +0530

    first

---
+++ problem6.py
@@ -0,0 +1,5 @@
+a, b = 2, 3
+c, b = a, c + 1
+print a, b, c
+
+#name error c is not defined
---
+++ quotes.py
@@ -0,0 +1,13 @@
+s = 'single quote\n'
+d = "double quotes\n"
+td = """string written in
+         two lines.."""
+print s + d + td
+
+a = "i like to use"
+b = "symbol like strings in single " '\'quotes\''
+c = '''\nand
+  and..'''
+print a, b, c
+
+
